cb0a10ea5b07365488be9e324126d76f6f5e9394
manhlinhhumg1985 <manhlinh85humg@gmail.com>
2017-06-22 11:26:03 +0700

    update1

--- index.js
+++ index.js
@@ -17,8 +17,7 @@ server.listen(3000);
 //lang nghe co nguoi ket noi len server
 let mangUser = [];
 let mangnguoichoi = [];
-//Tao ra mang ban co trong do cac cell co gia tri ban dau la 0
-//muc dich tao ra ma tran ban co nay de xet thang thua cho nguoi choi
+//Tao ra mang ban co trong do cac cell co gia tri ban dau la 0 muc dich tao ra ma tran ban co nay de xet thang thua cho nguoi choi
 //dung method cua Array
 Array.matrix = function (n, init) {
     let mat = [];
@@ -43,41 +42,51 @@ let Arr_Board = Array.matrix(8, 0)
  [ 0, 0, 0, 0, 0, 0, 0, 0 ] ]*/
 //Kiem tra thang thua khi nguoi choi danh nuoc moi tren ban co
 //Theo phương thẳng đứng phía trên
- let Vertically_up = (Matrix,Cur_row,Cur_col, value) =>{
-     let count =0;
-     for(let i= Cur_row; i>=0; i--){
-         if(Matrix[Cur_row][Cur_col]===value){
-             count ++;
-         }
-     }
-     return count;
- }
- //Theo phương thẳng đứng phía duoi
-let Vertically_down = (Cur_row, Cur_col) =>{
+let Vertically_up = (Matrix, Cur_row, Cur_col, value) => {
 
 }
+//Theo phương thẳng đứng phía duoi
+let Vertically_down = (Mat, Cur_row, Cur_col, Value) => {
+    let count = 0;
+    for (let i = 0; i <5; i++) {
+        if (Mat[i][Cur_col] === Value) {
+            count++;
+            if(count ===5){
+                return 1;
+            }
+        }
+    }
+}
 //Theo phương ngang bên phải
- let Horizontal_right = (Cur_row, Cur_col)=>{
-
- }
- //Theo phương ngang bên trái
-let Horizontal_left = (Cur_row, Cur_col)=>{
+function Horizontal_right (Mat, Cur_row, Cur_col, Value) {
+    let count = 0;
+    for (let i = 0; i < 5; i++) {
+        if (Mat[Cur_row][Cur_col+i] === Value) {
+            count++;
+            if (count === 5) {
+                return 1;
+            }
+        }
+    }
+}
+//Theo phương ngang bên trái
+let Horizontal_left = (Cur_row, Cur_col) => {
 
 }
 //Theo phương đường chéo phía trên bên phải
-let Diagonal_right_up =(Cur_row, Cur_col)=>{
+let Diagonal_right_up = (Cur_row, Cur_col) => {
 
 }
 //Theo phương đường chéo phía dưới bên phải
-let Diagonal_right_down =(Cur_row, Cur_col)=>{
+let Diagonal_right_down = (Cur_row, Cur_col) => {
 
 }
 //Theo phương đường chéo phía trên bên trái
-let Diagonal_left_up =(Cur_row, Cur_col)=>{
+let Diagonal_left_up = (Cur_row, Cur_col) => {
 
 }
 //Theo phương đường chéo phía dưới bên trái
-let Diagonal_left_down =(Cur_row, Cur_col)=>{
+let Diagonal_left_down = (Cur_row, Cur_col) => {
 
 }
 //Ket thuc
@@ -96,14 +105,13 @@ io.on("connection", function (socket) {
     })
     socket.on("su-kien-click", function (data) {
         let vitri = mangUser.indexOf(socket.Username)
-        let Columb = data.x /50;
-        let  Row= data.y/50;
-
+        let Columb = data.x / 50;
+        let Row = data.y / 50;
         //Kiem tra khong cho nguoi choi gui du lieu 2 lan lien tuc len server
         if (socket.Username !== mangnguoichoi[0]) {
             mangnguoichoi.unshift(socket.Username);
             if (vitri === 0) {
-                if(Arr_Board[Row][Columb] ===0){
+                if (Arr_Board[Row][Columb] === 0) {
                     Arr_Board[Row][Columb] = 1;
                     io.sockets.emit("server-send-data", {
                         name: socket.Username,
@@ -111,30 +119,38 @@ io.on("connection", function (socket) {
                         y: data.y,
                         nguoichoi: vitri,
                         ArrId: mangnguoichoi,
-                        Board : Arr_Board,
+                        Board: Arr_Board,
                         value: 1
                     })
                     //check ma tran hien tai cua ban co
-                    let string =''
-                    let count =0;
-                    for(let i=0; i< 6; i++){
-                        if(Arr_Board[Row][i]===1){
+                    //Kiem tra so luong quan cung mau tren hang ngang tu trai sang phai
+                    let string = ''
+                    let count = 0;
+                    /*for (let i = 0; i < 6; i++) {
+                        if (Arr_Board[Row][i] === 1) {
                             count++;
-                            if(count===5) {
-                                string ="BAN DA THUA"
+                            if (count === 5) {
+                                string = "BAN DA THUA"
                                 socket.broadcast.emit("khong-cho-doi-thu-click-khi-thua")
+                                socket.broadcast.emit("phat-su-kien-thang-thua", string)
                             }
-
                         }
+                    }*/
+                    if(Horizontal_right(Arr_Board,Row,Columb,1)){
+                        string ="BAN DA THUA CUOC";
+                        socket.broadcast.emit("khong-cho-doi-thu-click-khi-thua");
+                        socket.broadcast.emit("phat-su-kien-thang-thua", string)
                     }
-
-                    socket.broadcast.emit("phat-su-kien-thang-thua",string)
-
+                    if(Vertically_down(Arr_Board,Row,Columb,1)){
+                        string ="BAN DA THUA CUOC";
+                        socket.broadcast.emit("khong-cho-doi-thu-click-khi-thua");
+                        socket.broadcast.emit("phat-su-kien-thang-thua", string)
+                    }
                 }
             }
             else {
-                if(Arr_Board[Row][Columb]===0){
-                    Arr_Board[Row][Columb] =2;
+                if (Arr_Board[Row][Columb] === 0) {
+                    Arr_Board[Row][Columb] = 2;
                     io.sockets.emit("server-send-data", {
                         name: socket.Username,
                         x: data.x,
@@ -142,28 +158,35 @@ io.on("connection", function (socket) {
                         nguoichoi: vitri,
                         ArrId: mangnguoichoi,
                         Board: Arr_Board,
-                        value:2
-                })
-                    let string =''
-                    let count =0;
-                    for(let i=0; i< 6; i++){
-                        if(Arr_Board[Row][i]===2){
-                            count++;
-                            if(count===5) {
-                                string ="BAN DA THUA"
+                        value: 2
+                    })
+                    let string = ''
+                    let count1 = 0;
+                    /*for (let i = 0; i < 6; i++) {
+                        if (Arr_Board[Row][i] === 2) {
+                            count1++;
+                            if (count1 === 5) {
+                                string = "BAN DA THUA"
                                 socket.broadcast.emit("khong-cho-doi-thu-click-khi-thua")
+                                socket.broadcast.emit("phat-su-kien-thang-thua", string)
                             }
 
                         }
+                    }*/
+                    if(Horizontal_right(Arr_Board,Row,Columb,2)){
+                        string ="BAN DA THUA CUOC";
+                        socket.broadcast.emit("khong-cho-doi-thu-click-khi-thua");
+                        socket.broadcast.emit("phat-su-kien-thang-thua", string)
                     }
-                    socket.broadcast.emit("phat-su-kien-thang-thua",string)
-
+                    if(Vertically_down(Arr_Board,Row,Columb,2)){
+                        string ="BAN DA THUA CUOC";
+                        socket.broadcast.emit("khong-cho-doi-thu-click-khi-thua");
+                        socket.broadcast.emit("phat-su-kien-thang-thua", string)
+                    }
                 }
-
             }
         }
     })
-
 })
 //Tao 1 routter
 app.get("/", function (req, res) {
